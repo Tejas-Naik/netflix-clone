06339df90bd462be03c43362f9042620521515aa
Tejas-Naik <rntejas2005@gmail.com>
2022-12-24 01:27:37 -0800

    Making request to the API and rendering the images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import axios from "axios";
 import React from "react";
 import './App.css';
+import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey CleverProgrammer! Lets build Netflix Clone</h1>
+      <h1>Hey There! Lets build netflix clone</h1>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
     </div>
   );
 }
---
+++ src/components/Row.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import "../Row.css"
+
+function Row(props) {
+    const baseUrl = "https://image.tmdb.org/t/p/original"
+    const { fetchUrl } = props
+    const [movies, setMovies] = useState([]);
+    // UseEffect is going to run when the state of variable changes 2 params (callBkFn, [state]);
+    useEffect(() => {
+        // if [] means run one and var we can run this fn every time the var changes
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.table(movies);
+
+    return (
+        <div className='row'>
+            <h2>{props.title}</h2>
+            <div className="row__posters">
+                {/* several row__posters */}
+                {movies.map(movie => (
+                    <img
+                        className="row__poster"
+                        src={baseUrl + movie.poster_path} alt={movie.name} />
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default Row
