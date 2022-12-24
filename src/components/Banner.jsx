54eb68b7abe8bdc6eac58e34d042ac72f359991e
Tejas-Naik <rntejas2005@gmail.com>
2022-12-24 09:56:37 -0800

    Movie trailer added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import "../Banner.css";
 import requests from '../requests';
-
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube';
 function Banner() {
     const baseUrl = "https://image.tmdb.org/t/p/original"
     const [movie, setMovie] = useState([]);
@@ -19,6 +20,27 @@ function Banner() {
     }, [])
 
     console.log(movie);
+    // Playing trialer
+    const [trailerUrl, setTrailerUrl] = useState("");
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        }
+    };
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get("v"));
+                }).catch(err => console.error(err))
+        }
+    }
 
     function truncate(str, n) {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
@@ -40,7 +62,7 @@ function Banner() {
                     <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
                     {/* div > 2 buttons */}
                     <div className='banner__buttons'>
-                        <button className='banner__button'>Play</button>
+                        <button onClick={() => handleClick(movie)} className='banner__button'>Play</button>
                         <button className='banner__button'>My List</button>
                     </div>
                     <p className='banner__description'>{truncate(movie?.overview, 150)}</p>
@@ -48,6 +70,7 @@ function Banner() {
 
             </header>
             <div className='banner--fadeBottom'></div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import movieTrailer from 'movie-trailer';
 import React, { useEffect, useState } from 'react';
+import YouTube from 'react-youtube';
 import "../Row.css";
 
 function Row(props) {
@@ -17,6 +19,29 @@ function Row(props) {
         fetchData();
     }, [fetchUrl]);
 
+    // For playing trailer
+    const [trailerUrl, setTrailerUrl] = useState("");
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        }
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get("v"));
+                }).catch(err => console.error(err))
+        }
+    }
+
     return (
         <div className='row'>
             <h2>{props.title}</h2>
@@ -24,12 +49,13 @@ function Row(props) {
                 {/* several row__posters */}
                 {movies.map((movie) => (
                     <img
+                        onClick={() => handleClick(movie)}
                         key={movie.id}
                         className={`row__poster ${props.isLargeRow && "row__posterLarge"}`}
                         src={`${baseUrl}${props.isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
                 ))}
             </div>
-
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
